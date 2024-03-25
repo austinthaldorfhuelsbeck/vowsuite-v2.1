21527b5f15c1522ac87f300404b7913248bf2008
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2024-03-25 12:32:52 -0700

    get it to build!

--- src/app/studio/settings/team/[[...team]]/page.tsx
+++ src/app/studio/settings/team/[[...team]]/page.tsx
@@ -1,23 +1,4 @@
-import { currentUser } from "@clerk/nextjs";
-import ServerError from "~/components/global/server-error";
-import { api } from "~/trpc/server";
-
 export default async function TeamSettings() {
-  const user = await currentUser();
-
-  const email = user?.emailAddresses[0]?.emailAddress;
-
-  const userFromDb = await api.user.getByEmail({ email: email ?? "" });
-
-  if (!userFromDb?.agency) {
-    return (
-      <ServerError
-        code={500}
-        message={"Something went wrong. Please try again."}
-      />
-    );
-  }
-
   return (
     <div>
       <h1>Team Settings</h1>
