89db1a6c245cf1874fca00a05b5cc93f0505061c
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2024-03-25 12:24:41 -0700

    temporary to avoid error

--- src/app/studio/settings/agency/[[...agency]]/page.tsx
+++ src/app/studio/settings/agency/[[...agency]]/page.tsx
@@ -1,23 +1,3 @@
-import { currentUser } from "@clerk/nextjs";
-import ServerError from "~/components/global/server-error";
-import { api } from "~/trpc/server";
-import AgencyForm from "../_components/agency-form";
-
 export default async function AgencySettings() {
-  const user = await currentUser();
-
-  const email = user?.emailAddresses[0]?.emailAddress;
-
-  const userFromDb = await api.user.getByEmail({ email: email ?? "" });
-
-  if (!userFromDb?.agency) {
-    return (
-      <ServerError
-        code={500}
-        message={"Something went wrong. Please try again."}
-      />
-    );
-  }
-
-  return <AgencyForm agency={userFromDb.agency} />;
+  return <p>Agency settings</p>
 }
